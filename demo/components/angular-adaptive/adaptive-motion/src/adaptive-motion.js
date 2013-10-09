54dabf3ebcab626d4bfd72b828052273577e5e6a
Jan Antala <jan@antala.sk>
2013-10-09 23:11:31 +0200

    broadcast swipe events, public swipe functions

--- demo/components/angular-adaptive/adaptive-motion/src/adaptive-motion.js
+++ demo/components/angular-adaptive/adaptive-motion/src/adaptive-motion.js
@@ -48,108 +48,7 @@ adaptive.provider('$motion', [function() {
   var _ = canvas.getContext('2d');
   var c_ = ccanvas.getContext('2d');
 
-  var compression = 5;
-  var width = 0;
-  var height = 0;
-
-  var draw;
-  var localMediaStream;
-
-  var start = function(){
-    console.log('start');
-
-    window.URL = window.URL || window.webkitURL;
-    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
-
-    if (navigator.getUserMedia) {
-      navigator.getUserMedia({audio: false, video: true},
-        function(stream){
-          localMediaStream = stream;
-          video.src = window.URL.createObjectURL(stream);
-          video.addEventListener('play', function() {
-            window.setTimeout(function(){
-              requestId = window.requestAnimationFrame(dump);
-            }, 1000);
-          });
-        },
-        function(){
-          console.error('Denied!');
-        }
-      );
-    }
-    else {
-      console.error('getUserMedia() is not supported in your browser');
-    }
-  };
-
-  var stop = function(){
-    window.cancelAnimationFrame(requestId);
-    localMediaStream && localMediaStream.stop();
-    localMediaStream = undefined;
-  };
-
-  var dump = function() {
-    if (canvas.width !== video.videoWidth){
-      width = Math.floor(video.videoWidth / compression);
-      height = Math.floor(video.videoHeight / compression);
-      canvas.width = ccanvas.width = width;
-      canvas.height = ccanvas.height = height;
-    }
-
-    _.drawImage(video,0,0,width,height);
-    draw = _.getImageData(0, 0, width, height);
-    // c_.putImageData(draw,0,0);
-    skinfilter();
-    test();
-
-    requestId = window.requestAnimationFrame(dump);
-  };
-
-  var huemin = 0.0;
-  var huemax = 0.10;
-  var satmin = 0.0;
-  var satmax = 1.0;
-  var valmin = 0.4;
-  var valmax = 1.0;
-
-  var skinfilter = function(){
-
-    var skin_filter = _.getImageData(0,0,width,height);
-    var total_pixels = skin_filter.width * skin_filter.height;
-    var index_value = total_pixels * 4;
-
-    var count_data_big_array = 0;
-    for (var y=0; y<height; y++)
-    {
-      for (var x=0; x<width; x++)
-      {
-        index_value = x+y*width;
-        var r = draw.data[count_data_big_array];
-        var g = draw.data[count_data_big_array+1];
-        var b = draw.data[count_data_big_array+2];
-        var a = draw.data[count_data_big_array+3];
-
-        var hsv = rgb2Hsv(r,g,b);
-        //When the hand is too lose (hsv[0] > 0.59 && hsv[0] < 1.0)
-        //Skin Range on HSV values
-        if(((hsv[0] > huemin && hsv[0] < huemax)||(hsv[0] > 0.59 && hsv[0] < 1.0))&&(hsv[1] > satmin && hsv[1] < satmax)&&(hsv[2] > valmin && hsv[2] < valmax)){
-          skin_filter[count_data_big_array] = r;
-          skin_filter[count_data_big_array+1] = g;
-          skin_filter[count_data_big_array+2] = b;
-          skin_filter[count_data_big_array+3] = a;
-        }
-        else{
-          skin_filter.data[count_data_big_array] = 0;
-          skin_filter.data[count_data_big_array+1] = 0;
-          skin_filter.data[count_data_big_array+2] = 0;
-          skin_filter.data[count_data_big_array+3] = 0;
-        }
-
-        count_data_big_array = index_value * 4;
-      }
-    }
-    draw = skin_filter;
-  };
+  
 
   var rgb2Hsv = function(r, g, b){
 
@@ -181,126 +80,268 @@ adaptive.provider('$motion', [function() {
     return [h, s, v];
   };
 
-  var last = false;
-  var thresh = 150;
-  var down = false;
-  var wasdown = false;
+  
 
-  var test = function(){
-    var delt = _.createImageData(width, height);
-    var totalx = 0;
-    var totaly = 0;
-    var totald = 0;
-    var totaln = delt.width * delt.height;
-    var dscl = 0;
-    var pix = totaln * 4;
 
-    if(last !== false){
+  this.$get = function($rootScope) {
 
-      while ((pix -= 4) > 0) {
-        var d = Math.abs(draw.data[pix] - last.data[pix]) +
-                Math.abs(draw.data[pix+1] - last.data[pix+1]) +
-                Math.abs(draw.data[pix+2]-last.data[pix+2]);
+    var compression = 5;
+    var width = 0;
+    var height = 0;
 
-        if (d > thresh){
-          delt.data[pix] = 160;
-          delt.data[pix+1] = 255;
-          delt.data[pix+2] = 255;
-          delt.data[pix+3] = 255;
-          totald += 1;
-          totalx += (pix/4) % width;
-          totaly += Math.floor((pix/4) / delt.height);
-        }
-        else {
-          delt.data[pix] = 0;
-          delt.data[pix+1] = 0;
-          delt.data[pix+2] = 0;
-          delt.data[pix+3] = 0;
+    var draw;
+    var localMediaStream;
+
+    var start = function(){
+      console.log('start');
+
+      window.URL = window.URL || window.webkitURL;
+      navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+      if (navigator.getUserMedia) {
+        navigator.getUserMedia({audio: false, video: true},
+          function(stream){
+            localMediaStream = stream;
+            video.src = window.URL.createObjectURL(stream);
+            video.addEventListener('play', function() {
+              window.setTimeout(function(){
+                requestId = window.requestAnimationFrame(dump);
+              }, 1000);
+            });
+          },
+          function(){
+            console.error('Denied!');
+          }
+        );
+      }
+      else {
+        console.error('getUserMedia() is not supported in your browser');
+      }
+    };
+
+    var stop = function(){
+      window.cancelAnimationFrame(requestId);
+      localMediaStream && localMediaStream.stop();
+      localMediaStream = undefined;
+    };
+
+    var dump = function() {
+      if (canvas.width !== video.videoWidth){
+        width = Math.floor(video.videoWidth / compression);
+        height = Math.floor(video.videoHeight / compression);
+        canvas.width = ccanvas.width = width;
+        canvas.height = ccanvas.height = height;
+      }
+
+      _.drawImage(video,0,0,width,height);
+      draw = _.getImageData(0, 0, width, height);
+      // c_.putImageData(draw,0,0);
+      skinfilter();
+      test();
+
+      requestId = window.requestAnimationFrame(dump);
+    };
+
+    var huemin = 0.0;
+    var huemax = 0.10;
+    var satmin = 0.0;
+    var satmax = 1.0;
+    var valmin = 0.4;
+    var valmax = 1.0;
+
+    var skinfilter = function(){
+
+      var skin_filter = _.getImageData(0,0,width,height);
+      var total_pixels = skin_filter.width * skin_filter.height;
+      var index_value = total_pixels * 4;
+
+      var count_data_big_array = 0;
+      for (var y=0; y<height; y++)
+      {
+        for (var x=0; x<width; x++)
+        {
+          index_value = x+y*width;
+          var r = draw.data[count_data_big_array];
+          var g = draw.data[count_data_big_array+1];
+          var b = draw.data[count_data_big_array+2];
+          var a = draw.data[count_data_big_array+3];
+
+          var hsv = rgb2Hsv(r,g,b);
+          //When the hand is too lose (hsv[0] > 0.59 && hsv[0] < 1.0)
+          //Skin Range on HSV values
+          if(((hsv[0] > huemin && hsv[0] < huemax)||(hsv[0] > 0.59 && hsv[0] < 1.0))&&(hsv[1] > satmin && hsv[1] < satmax)&&(hsv[2] > valmin && hsv[2] < valmax)){
+            skin_filter[count_data_big_array] = r;
+            skin_filter[count_data_big_array+1] = g;
+            skin_filter[count_data_big_array+2] = b;
+            skin_filter[count_data_big_array+3] = a;
+          }
+          else{
+            skin_filter.data[count_data_big_array] = 0;
+            skin_filter.data[count_data_big_array+1] = 0;
+            skin_filter.data[count_data_big_array+2] = 0;
+            skin_filter.data[count_data_big_array+3] = 0;
+          }
+
+          count_data_big_array = index_value * 4;
         }
       }
-    }
-
-    if (totald){
-      down = {
-        x: totalx / totald,
-        y: totaly / totald,
-        d: totald
-      };
-      handleGesture();
-    }
-
-    last = draw;
-    c_.putImageData(delt,0,0);
-  };
-
-  var movethresh = 2;
-  var brightthresh = 300;
-  var overthresh = 1000;
-
-  var calibrate = function(){
-    wasdown = {
-      x: down.x,
-      y: down.y,
-      d: down.d
+      draw = skin_filter;
     };
-  };
 
-  var avg = 0;
-  var state = 0; //States: 0 waiting for gesture, 1 waiting for next move after gesture, 2 waiting for gesture to end
+    var last = false;
+    var thresh = 150;
+    var down = false;
+    var wasdown = false;
 
-  var handleGesture = function(){
-    avg = 0.9 * avg + 0.1 * down.d;
-    var davg = down.d - avg;
-    var good = davg > brightthresh;
+    var test = function(){
+      var delt = _.createImageData(width, height);
+      var totalx = 0;
+      var totaly = 0;
+      var totald = 0;
+      var totaln = delt.width * delt.height;
+      var dscl = 0;
+      var pix = totaln * 4;
 
-    switch (state){
-      case 0:
-        if (good){ //Found a gesture, waiting for next move
-          state = 1;
-          calibrate();
-        }
-        break;
-      case 2: //Wait for gesture to end
-        if (!good){ //Gesture ended
-          state = 0;
-        }
-        break;
-      case 1: //Got next move, do something based on direction
-        var dx = down.x - wasdown.x;
-        var dy = down.y - wasdown.y;
-        var dirx = Math.abs(dy) < Math.abs(dx); //(dx,dy) is on a bowtie
-        console.log(dx, dy, dirx);
+      if(last !== false){
 
-        if (dirx) {
-          if (dx < - movethresh){
-            console.log('Swipe right.');
+        while ((pix -= 4) > 0) {
+          var d = Math.abs(draw.data[pix] - last.data[pix]) +
+                  Math.abs(draw.data[pix+1] - last.data[pix+1]) +
+                  Math.abs(draw.data[pix+2]-last.data[pix+2]);
+
+          if (d > thresh){
+            delt.data[pix] = 160;
+            delt.data[pix+1] = 255;
+            delt.data[pix+2] = 255;
+            delt.data[pix+3] = 255;
+            totald += 1;
+            totalx += (pix/4) % width;
+            totaly += Math.floor((pix/4) / delt.height);
           }
-          else if (dx > movethresh){
-            console.log('Swipe left.');
-          }
-        }
-        else {
-          if (dy > movethresh){
-            console.log('Swipe down.');
-          }
-          else if (dy < - movethresh){
-            console.log('Swipe up.');
+          else {
+            delt.data[pix] = 0;
+            delt.data[pix+1] = 0;
+            delt.data[pix+2] = 0;
+            delt.data[pix+3] = 0;
           }
         }
+      }
 
-        state = 2;
-        break;
-    }
-  };
+      if (totald){
+        down = {
+          x: totalx / totald,
+          y: totaly / totald,
+          d: totald
+        };
+        handleGesture();
+      }
 
+      last = draw;
+      c_.putImageData(delt,0,0);
+    };
 
-  this.$get = function() {
+    var movethresh = 2;
+    var brightthresh = 300;
+    var overthresh = 1000;
+
+    var calibrate = function(){
+      wasdown = {
+        x: down.x,
+        y: down.y,
+        d: down.d
+      };
+    };
+
+    var avg = 0;
+    var state = 0; //States: 0 waiting for gesture, 1 waiting for next move after gesture, 2 waiting for gesture to end
+
+    var handleGesture = function(){
+      avg = 0.9 * avg + 0.1 * down.d;
+      var davg = down.d - avg;
+      var good = davg > brightthresh;
+
+      switch (state){
+        case 0:
+          if (good){ //Found a gesture, waiting for next move
+            state = 1;
+            calibrate();
+          }
+          break;
+        case 2: //Wait for gesture to end
+          if (!good){ //Gesture ended
+            state = 0;
+          }
+          break;
+        case 1: //Got next move, do something based on direction
+          var dx = down.x - wasdown.x;
+          var dy = down.y - wasdown.y;
+          var dirx = Math.abs(dy) < Math.abs(dx); //(dx,dy) is on a bowtie
+          console.log(dx, dy, dirx);
+
+          if (dirx) {
+            if (dx < - movethresh){
+              $rootScope.$broadcast('adaptive.motion:onSwipeRight');
+            }
+            else if (dx > movethresh){
+              $rootScope.$broadcast('adaptive.motion:onSwipeLeft');
+            }
+          }
+          else {
+            if (dy > movethresh){
+              $rootScope.$broadcast('adaptive.motion:onSwipeDown');
+            }
+            else if (dy < - movethresh){
+              $rootScope.$broadcast('adaptive.motion:onSwipeUp');
+            }
+          }
+
+          state = 2;
+          break;
+      }
+    };
+
+    var onSwipeLeft = function(cb){
+      $rootScope.$on('adaptive.motion:onSwipeLeft', function(e, data){
+        cb(data);
+      });
+    };
+
+    var onSwipeRight = function(cb){
+      $rootScope.$on('adaptive.motion:onSwipeRight', function(e, data){
+        cb(data);
+      });
+    };
+
+    var onSwipeUp = function(cb){
+      $rootScope.$on('adaptive.motion:onSwipeUp', function(e, data){
+        cb(data);
+      });
+    };
+
+    var onSwipeDown = function(cb){
+      $rootScope.$on('adaptive.motion:onSwipeDown', function(e, data){
+        cb(data);
+      });
+    };
+
     return {
       start: function(){
         start();
       },
       stop: function(){
         stop();
+      },
+      onSwipeLeft: function(cb){
+        onSwipeLeft(cb);
+      },
+      onSwipeRight: function(cb){
+        onSwipeRight(cb);
+      },
+      onSwipeUp: function(cb){
+        onSwipeUp(cb);
+      },
+      onSwipeDown: function(cb){
+        onSwipeDown(cb);
       }
     };
   };
--- demo/js/controllers/mainCtrl.js
+++ demo/js/controllers/mainCtrl.js
@@ -7,6 +7,23 @@
  */
 motion.controller('mainCtrl', function mainCtrl($scope, $rootScope, $motion) {
   $motion.start();
+
+  $motion.onSwipeLeft(function(data){
+    console.log('onSwipeLeft');
+  });
+
+  $motion.onSwipeRight(function(data){
+    console.log('onSwipeRight');
+  });
+
+  $motion.onSwipeUp(function(data){
+    console.log('onSwipeUp');
+  });
+
+  $motion.onSwipeDown(function(data){
+    console.log('onSwipeDown');
+  });
+
 });
 
 })();
