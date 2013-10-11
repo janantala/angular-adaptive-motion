9272d9979d91615a24446940bb008f0de335e2c8
Jan Antala <jan@antala.sk>
2013-10-11 15:50:44 +0200

    play sound

--- demo/js/controllers/mainCtrl.js
+++ demo/js/controllers/mainCtrl.js
@@ -6,22 +6,37 @@
  * The main controller for the app. 
  */
 motion.controller('mainCtrl', function mainCtrl($scope, $rootScope, $motion) {
+
+  var playSound = function(){
+    var audio = new Audio('/assets/lightsaber.wav');
+    audio.play();
+  };
+
+
   $motion.start();
 
   $motion.onSwipeLeft(function(data){
     console.log('onSwipeLeft');
+    playSound();
+    $scope.$apply();
   });
 
   $motion.onSwipeRight(function(data){
     console.log('onSwipeRight');
+    playSound();
+    $scope.$apply();
   });
 
   $motion.onSwipeUp(function(data){
     console.log('onSwipeUp');
+    playSound();
+    $scope.$apply();
   });
 
   $motion.onSwipeDown(function(data){
     console.log('onSwipeDown');
+    playSound();
+    $scope.$apply();
   });
 
 });
