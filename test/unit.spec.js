35fb4649d9183a0b753bbbefb65476966e7c33ed
Jan Antala <jan@antala.sk>
2013-10-20 14:07:36 +0200

    update provider test spec

--- test/unit.spec.js
+++ test/unit.spec.js
@@ -1,93 +1,101 @@
 describe('adaptive.motion', function() {
 
-  var $motion;
+  describe('$motionProvider config', function() {
+    var $motion;
 
-  beforeEach(module('adaptive.motion', function($motionProvider) {
-    $motion = $motionProvider;
-  }));
+    beforeEach(module('adaptive.motion', function($motionProvider) {
+      $motion = $motionProvider;
+    }));
 
-  var rootscope;
-  beforeEach(inject(function($rootScope) {
-    rootScope = $rootScope;
-    spyOn(rootScope, "$on").andCallThrough();
-    spyOn(rootScope, "$broadcast").andCallThrough();
-  }));
+    it('should be defined', inject(function() {
+      expect($motion).toBeDefined();
+      expect(typeof $motion).toBe('object');
+    }));
 
-  it('should be defined', inject(function() {
-    expect($motion).toBeDefined();
-    expect(typeof $motion).toBe('object');
-  }));
+    it('should have default treshold options', inject(function() {
+      expect($motion.treshold).toBeDefined();
+      expect($motion.treshold.rgb).toBeDefined();
+      expect($motion.treshold.move).toBeDefined();
+      expect($motion.treshold.bright).toBeDefined();
+    }));
 
-  it('should have default treshold options', inject(function() {
-    expect($motion.treshold).toBeDefined();
-    expect($motion.treshold.rgb).toBeDefined();
-    expect($motion.treshold.move).toBeDefined();
-    expect($motion.treshold.bright).toBeDefined();
-  }));
+    it('should have setTreshold method', inject(function() {
+      expect($motion.setTreshold).toBeDefined();
+      expect(typeof $motion.setTreshold).toBe('function');
 
-  it('should have setTreshold method', inject(function() {
-    expect($motion.setTreshold).toBeDefined();
-    expect(typeof $motion.setTreshold).toBe('function');
+      $motion.setTreshold({
+        'rgb': 123
+      });
 
-    $motion.setTreshold({
-      'rgb': 123
-    });
+      expect($motion.treshold.rgb).toBe(123);
+    }));
 
-    expect($motion.treshold.rgb).toBe(123);
-  }));
+  });
 
-  it('should have public methods', inject(function() {
-    expect($motion.$get()).toBeDefined();
-    expect($motion.$get().start).toBeDefined();
-    expect($motion.$get().stop).toBeDefined();
-    expect($motion.$get().onSwipeLeft).toBeDefined();
-    expect($motion.$get().onSwipeRight).toBeDefined();
-    expect($motion.$get().onSwipeUp).toBeDefined();
-    expect($motion.$get().onSwipeDown).toBeDefined();
+  describe('$get methods', function() {
 
-    expect(typeof $motion.$get().start).toBe('function');
-    expect(typeof $motion.$get().stop).toBe('function');
-    expect(typeof $motion.$get().onSwipeLeft).toBe('function');
-    expect(typeof $motion.$get().onSwipeRight).toBe('function');
-    expect(typeof $motion.$get().onSwipeUp).toBe('function');
-    expect(typeof $motion.$get().onSwipeDown).toBe('function');
-  }));
+    beforeEach(module('adaptive.motion', function() {
 
-  it('should have called rootScope.$broadcast', function(){
+    }));
 
-    // var counter = 0;
+    it('should have public methods', inject(function($motion) {
+      expect($motion).toBeDefined();
+      expect($motion.start).toBeDefined();
+      expect($motion.stop).toBeDefined();
+      expect($motion.onSwipeLeft).toBeDefined();
+      expect($motion.onSwipeRight).toBeDefined();
+      expect($motion.onSwipeUp).toBeDefined();
+      expect($motion.onSwipeDown).toBeDefined();
 
-    // $motion.$get().onSwipeLeft(function(){
-    //   counter++;
-    // });
+      expect(typeof $motion.start).toBe('function');
+      expect(typeof $motion.stop).toBe('function');
+      expect(typeof $motion.onSwipeLeft).toBe('function');
+      expect(typeof $motion.onSwipeRight).toBe('function');
+      expect(typeof $motion.onSwipeUp).toBe('function');
+      expect(typeof $motion.onSwipeDown).toBe('function');
+    }));
 
-    // $motion.$get().onSwipeRight(function(){
-    //   counter++;
-    // });
+    it('should have called rootScope.$broadcast', inject(function($motion, $rootScope){
 
-    // $motion.$get().onSwipeUp(function(){
-    //   counter++;
-    // });
+      spyOn($rootScope, '$on').andCallThrough();
+      spyOn($rootScope, '$broadcast').andCallThrough();
+      $rootScope.$apply();
 
-    // $motion.$get().onSwipeDown(function(){
-    //   counter++;
-    // });
-    // 
+      var counter = 0;
 
-    rootScope.$broadcast('adaptive.motion:onSwipeLeft');
-    expect(rootScope.$broadcast).toHaveBeenCalled();
-    // expect(counter).toBe(1);
+      $motion.onSwipeLeft(function(){
+        counter++;
+      });
 
-    rootScope.$broadcast('adaptive.motion:onSwipeRight');
-    expect(rootScope.$broadcast).toHaveBeenCalled();
-    // expect(counter).toBe(2);
+      $motion.onSwipeRight(function(){
+        counter++;
+      });
 
-    rootScope.$broadcast('adaptive.motion:onSwipeUp');
-    expect(rootScope.$broadcast).toHaveBeenCalled();
-    // expect(counter).toBe(3);
+      $motion.onSwipeUp(function(){
+        counter++;
+      });
 
-    rootScope.$broadcast('adaptive.motion:onSwipeDown');
-    expect(rootScope.$broadcast).toHaveBeenCalled();
-    // expect(counter).toBe(4);
+      $motion.onSwipeDown(function(){
+        counter++;
+      });
+      
+
+      $rootScope.$broadcast('adaptive.motion:onSwipeLeft');
+      expect($rootScope.$broadcast).toHaveBeenCalled();
+      expect(counter).toBe(1);
+
+      $rootScope.$broadcast('adaptive.motion:onSwipeRight');
+      expect($rootScope.$broadcast).toHaveBeenCalled();
+      expect(counter).toBe(2);
+
+      $rootScope.$broadcast('adaptive.motion:onSwipeUp');
+      expect($rootScope.$broadcast).toHaveBeenCalled();
+      expect(counter).toBe(3);
+
+      $rootScope.$broadcast('adaptive.motion:onSwipeDown');
+      expect($rootScope.$broadcast).toHaveBeenCalled();
+      expect(counter).toBe(4);
+    }));
+
   });
 });
